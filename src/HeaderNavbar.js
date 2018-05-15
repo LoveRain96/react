7d75934938e1afb7ab3bcb8f24d12bf203eed091
TienRyo <boy96totdong@gmail.com>
2018-05-15 17:23:26 +0700

    login

--- src/HeaderNavbar.js
+++ src/HeaderNavbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Input, Layout, Menu} from 'antd';
 import {Link}               from 'react-router-dom';
-const { Header} = Layout;
 const Search = Input.Search;
 
 export default class HeaderNavbar extends React.Component {
@@ -22,10 +21,11 @@ export default class HeaderNavbar extends React.Component {
                             style={{ width: 500, marginLeft : 100 }}
                         />
                     </Menu.Item>
-                    <Menu.Item key="2"><Link to='/courses'>COURSES</Link></Menu.Item>
-                    <Menu.Item key="3"><Link to='/companies'>COMPANIES</Link></Menu.Item>
-                    <Menu.Item key="4">CONTACT</Menu.Item>
-                    <Menu.Item key="5">LOGIN</Menu.Item>
+                    <Menu.Item key="2"><Link to='/'>NEW</Link></Menu.Item>
+                    <Menu.Item key="3"><Link to='/courses'>COURSES</Link></Menu.Item>
+                    <Menu.Item key="4"><Link to='/companies'>COMPANIES</Link></Menu.Item>
+                    <Menu.Item key="5">CONTACT</Menu.Item>
+                    <Menu.Item  key="6"><Link to={'/login'}>LOGIN</Link></Menu.Item>
                 </Menu>
             </div>
         )
